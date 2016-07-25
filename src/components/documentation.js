9b807e1405676cb3c9801d21c1ff1e2b1c7e604d
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-07-25 15:59:17 -0700

    render links without .md suffix

--- src/components/documentation.js
+++ src/components/documentation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MarkdownIt from "markdown-it";
+import MarkdownItNamedHeaders from "markdown-it-named-headers";
 
 class Documentation extends React.Component {
   componentWillMount() {
@@ -9,8 +10,29 @@ class Documentation extends React.Component {
   setMarkdownRender() {
     const md = new MarkdownIt({
       html: true,
-      linkify: true
+      linkify: true,
+      typographer: true
     });
+    md.use(MarkdownItNamedHeaders);
+
+    /* eslint-disable camelcase, max-params */
+    // store the original rule
+    const defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, renderer) {
+      return renderer.renderToken(tokens, idx, options);
+    };
+
+    // Update anchors that point to markdown files
+    md.renderer.rules.link_open = function (tokens, idx, options, env, renderer) {
+      const aIndex = tokens[idx].attrIndex("href");
+      if (aIndex >= 0) {
+        const mdHref = tokens[idx].attrs[aIndex][1].match(".md");
+        if (mdHref) {
+          tokens[idx].attrs[aIndex][1] = mdHref.input.substring(0, mdHref.index);
+        }
+      }
+      return defaultRender(tokens, idx, options, env, renderer);
+    };
+
     this.md = md;
   }
 
