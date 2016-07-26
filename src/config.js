8d6d49e545f863820caf9a571392aba3cf5f5d26
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-07-26 16:13:06 -0700

    load docs from one source

---
+++ src/config.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    label: "Webpack Code Splitting",
+    route: "/frontend/webpack-code-splitting"
+  },
+  {
+    label: "Webpack Plugins",
+    route: "/frontend/webpack-plugins"
+  },
+  {
+    label: "Webpack Shared Libraries",
+    route: "/frontend/webpack-shared-libs"
+  },
+  {
+    label: "Webpack Source Maps",
+    route: "/frontend/webpack-source-maps"
+  },
+  {
+    label: "Having a Single Infrastructure",
+    route: "/infrastructure/single"
+  }
+];
--- src/views/docs.js
+++ src/views/docs.js
@@ -1,33 +1,64 @@
 import React from "react";
 import Radium from "radium";
-import { includes } from "lodash";
+import { Link } from "react-router";
 import Page from "../components/page";
 import Documentation from "../components/documentation";
-import staticRoutes from "../../static-routes";
+import config from "../config";
 import NotFound from "./not-found";
 import theme from "../playbook-theme";
+import NextRead from "../components/next-read";
 
 const Docs = function (props) {
-  if (includes(staticRoutes, props.location.pathname)) {
-    const styles = {
-      main: {
-        width: "100%",
-        maxWidth: `calc(${theme.container} + ${theme.gutter})`,
-        margin: `${theme.gutter} auto`,
-        padding: theme.gutter
-      }
-    };
+  const styles = {
+    main: {
+      width: "100%",
+      maxWidth: `calc(${theme.container} + ${theme.gutter})`,
+      margin: `${theme.gutter} auto`,
+      padding: theme.gutter
+    },
+    inner: {
+      width: "100%",
+      maxWidth: `calc(${theme.container} + ${theme.gutter})`,
+      margin: `0px auto`
+    },
+    header: {
+      padding: theme.gutter,
+      backgroundColor: theme.red,
+      backgroundImage: "url('./static/hero.jpg')",
+      backgroundSize: "cover",
+      backgroundPosition: "center center",
+      color: theme.white,
+      textDecoration: "none",
+      textShadow: "2px 2px 8px rgba(168, 37, 27,.5)"
+    },
+    homeLink: {
+      fontSize: "1rem",
+      textDecoration: "none"
+    }
+  };
 
-    return (
-      <Page name="docs">
-        <main styles={styles.main}>
-          <Documentation markdown={require(`../../docs${props.location.pathname}.md`)} />
-        </main>
-      </Page>
-    );
-  } else {
-    return <NotFound />;
-  }
+  let Component = <NotFound />;
+
+  config.forEach((doc) => {
+    if (doc.route === props.location.pathname) {
+      Component = (
+        <Page name="docs">
+          <header style={styles.header}>
+            <div style={styles.inner}>
+              <h1 style={{fontSize: "3em"}}>Formidable Playbook</h1>
+              <div style={{fontSize: "1.2em", margin: ".25em 0em 1em"}}>A practical guide to building modern applications</div>
+            </div>
+          </header>
+          <main style={styles.main}>
+            <Link to="/" style={styles.homeLink}>&larr; Return Home</Link>
+            <Documentation markdown={require(`!!raw!../../docs${doc.route}.md`)}/>
+            <NextRead current={props.location.pathname} />
+          </main>
+        </Page>
+      );
+    }
+  });
+  return Component;
 };
 
 Docs.propTypes = {
