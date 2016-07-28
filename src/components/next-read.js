ea4cf93bc14c95ec81885674a240c0b8847615fc
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-07-28 10:44:11 -0700

    one more unused lodash method

--- src/components/next-read.js
+++ src/components/next-read.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Radium from "radium";
 import { Link } from "react-router";
-import { findIndex } from "lodash";
 import config from "../config";
 import theme from "../playbook-theme";
 
